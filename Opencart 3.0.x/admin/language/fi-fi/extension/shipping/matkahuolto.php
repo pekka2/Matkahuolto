cfec356fb85a416aee5f0ac341b2a6f0373f4a9b
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-12 18:21:13 +0300

    Create matkahuolto.php

---
+++ Opencart 3.0.x/admin/language/fi-fi/extension/shipping/matkahuolto.php
@@ -0,0 +1,55 @@
+<?php
+// Heading
+$_['heading_title']               = 'Matkahuolto';
+
+// Text
+$_['text_extension']              = 'Laajennukset';
+$_['text_success']                = 'Olet muokkanut Matkahuolto- toimitustapaa onnistuneesti!';
+$_['text_edit']                   = 'Muokkaa Matkahuolto-toimitustapaa';
+$_['text_hinnasto_description']   = 'Kotimaan hinnasto';
+$_['text_hinnasto_2_description'] = 'Hinnasto kohde Ruotsi ja Tanska';
+$_['text_hinnasto_3_description'] = 'Hinnaston kohde Baltian maat';
+$_['text_hinnasto_4_description'] = 'Hinnaston kohde Saksa, Itävalta ja Benelux maat';
+
+// Tab
+$_['tab_hinnasto']             = 'Hinnasto';
+$_['tab_hinnasto_2']           = 'Hinnasto 2';
+$_['tab_hinnasto_3']           = 'Hinnasto 3';
+$_['tab_hinnasto_4']           = 'Hinnasto 4';
+
+// Column
+$_['column_kg']                = 'Paino';
+$_['column_price']             = 'Hinta';
+
+// Button
+$_['button_weight_add']        = 'Lisää hintaraja';
+
+// Entry
+$_['entry_test_mode']          = 'Testaustila';
+$_['entry_user_id']            = 'Käyttäjä';
+$_['entry_tax_class']          = 'Veroluokka';
+$_['entry_length_class']       = 'Pituusluokka';
+$_['entry_weight_class']       = 'Painoluokka';
+$_['entry_geo_zone']           = 'Alue';
+$_['entry_status']             = 'Tila';
+$_['entry_sort_order']         = 'Järjestys';
+$_['entry_search']             = 'Hakutuloksia kassalla';
+$_['entry_free_cargo']         = 'Rahtivapaa';
+$_['entry_cargo_sum']          = 'Rahtivapaus/<br>Rahtialennus raja/€';
+$_['entry_discount_cargo']     = 'Alennus rahdista';
+$_['entry_discount_cargo_percent'] = 'Rahdin alennus prosentti';
+$_['entry_iso_palvelu']        = 'Iso-palvelu';
+$_['entry_iso_add_price']      = 'Iso-palvelun lisähinta';
+$_['entry_kotijakelu_price']   = 'Kotijakelun lisähinta';
+$_['entry_kotijakelu']         = 'Kotijakelu';
+$_['entry_hinnasto_2']         = 'Hinnasto 2';
+$_['entry_hinnasto_3']         = 'Hinnasto 3';
+$_['entry_hinnasto_4']         = 'Hinnasto 4';
+
+// Help
+$_['help_iso_palvelu']         = 'Matkahuollon Iso-lisäpalvelun avulla voit lähettää isommat, enintään 40 x 40 x 300 cm tai 70 x 80 x 150 cm kokoiset lähetykset';
+$_['help_iso_price']           = 'Matkahuollon sivustolla oleva hinta tai Matkahuollon kanssa sovittu hinta Iso-lisäpalvelulle (ilman ALV)';
+$_['help_kotijakelu_price']    = 'Lisähinta kotijakelulle. Tämä voi olla myös sinun itsesi määrittelemä lisähinta (ilman ALV)';
+// Error
+$_['error_user_id']     = 'Virhe: Käyttäjätunnus vaaditaan!';
+$_['error_permission'] = 'Sinulla ei ole oieutta muokata Matkahuolto- toimitustapaa!';
